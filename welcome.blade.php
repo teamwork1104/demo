80f01ec2a7811189845e25f97038d877548ccf60
Anoob <54050679+Anoob007@users.noreply.github.com>
2020-07-05 13:13:46 +0530

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Buskey - Corporate Business Template">
+    <title>Thrilok IT Services</title>
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+    <link href="css/plugins.min.css" rel="stylesheet" />
+    <link href="css/flaticon-business-set.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,900" rel="stylesheet">
+      <!--new code on google -->  
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156169489-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-156169489-1');
+</script>
+
+</head>
+<body>
+   
+
+   
+    <header>
+       
+        <nav class="navbar navbar-default navbar-sticky bootsnav">
+            <div class="container">
+                
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand" href="index.html">
+                        <img src="img/logo.png" class="logo logo-display" alt="Logo">
+                    </a>
+                </div>
+              
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <ul class="nav navbar-nav navbar-right" data-in="#" data-out="#">
+           <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                       
+                       
+                       <li>
+                            <a href="vr-ar-development.html">VR-AR</a>
+                        </li>
+                        <li>
+                            <a href="games.html">Games</a>
+                        </li>
+                        
+                        <li>
+                            <a href="portfolio.html">Portfolio</a>
+                        </li>
+                       <!--  <li>
+                            <a href="it-trainings.html">Trainings</a>
+                        </li>
+                       
+                       <li>
+                            <a href="careers.html">Careers</a>
+                        </li>-->
+                        <li>
+                            <a href="services.html">Services</a>
+                        </li> <li>
+                            <a href="contact.html">Contact </a>
+                        </li> <li>
+                            <a href="about.html">About</a>
+                        </li>
+                         <li>
+                            <a href="job.html">Career</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+    </header>
+    
+    
+    <div class="breadcrumb-area shadow dark bg-fixed text-center padding-xl text-light" style="background-image: url(img/banner/10.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 text-left">
+                    <h1>Career</h1>
+                </div>
+                <div class="col-md-6 col-sm-6 text-right">
+                    <ul class="breadcrumb">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Page</a></li>
+                        <li class="active">Career</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+    <div class="contact-form-area default-padding">
+        <div class="container">
+            <div class="row">
+                <!-- Start Contact Form -->
+                <div class="col-md-7">
+                    <div class="content">
+                        <div class="heading">
+                            <h3>Register Now</h3>
+                        </div>
+                       <form action="cre" method="post" enctype="multipart/form-data">
+						<?php echo csrf_field(); ?>
+						   <label for="exampleForm2">Name</label>
+                           <input type="text" name="name" id="exampleForm2" class="form-control">
+						<label for="exampleForm2">Email</label>
+                           <input type="email" name="email" id="exampleForm2" class="form-control">
+						   <label for="exampleForm2">Phone</label>
+                           <input type="number" name="phone" id="exampleForm2" class="form-control">
+						     <label for="exampleForm2">Qualification</label>
+                          <select class="form-control" name="select">
+							<option>Select</option>
+						   <option>Diploma</option>
+						   <option>UG</option>
+						   <option>PG</option>
+						   </select>
+						   <label for="exampleForm2">Resume</label>
+                           <input type="file" name="file" id="exampleForm2" class="form-control file">
+						   
+						   
+						   <br>
+						<button class="btn btn-danger">
+						   
+						  Submit
+						   </button>
+						
+						
+						</form>
+                       
+                    </div>
+                </div>
+                <!-- End Contact Form -->
+                <div class="col-md-5 office-info">
+                    <!-- Start Tab Contact Info -->
+                    <div class="tab-nvas">
+                        <ul class="nav nav-pills">
+                            <li class="active">
+                               <!-- <a data-toggle="tab" href="#id1" aria-expanded="true"></a>-->
+                                <h1><b>   JOB FAIR </h1>
+                                
+                            </li>
+                         
+                        </ul>
+                    </div>
+                    <div class="tab-content pad-all-20p txt-center-mobile">
+                        <div id="id1" class="tab-pane active">
+                            <ul>
+                                <li>
+                                    <!--<img src="assets/img/office/1.jpg" alt="Thumb"> -->
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-map-marker-alt"></i>
+                                    </div>
+                                    <div class="info">
+                                        <p>
+                                            Our Location
+                                            <span>Thrilok IT Services,
+                                            Kombara Marriott(2nd Floor),</span>
+                                            <span>St.Benedict Road,
+                                            High Court,Kochi,682018</span>
+                                        </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-envelope-open"></i>
+                                    </div>
+                                    <div class="info">
+                                        <p>
+                                            Send Us Mail
+                                            <span>Info@thrilok.com</span>
+                                        </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-mobile-alt"></i>
+                                    </div>
+                                    <div class="info">
+                                        <p>
+                                            Call Us
+                                            <span>0484 4876565</span>
+                                            <span>9447336638</span>
+                                        </p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+       
+                        
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+    <!--<div class="maps-area-items">
+        <div class="maps-box oh">
+            <div class="google-maps">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.333322261943!2d76.27476831428191!3d9.989300775960455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080d3e93d1040d%3A0xf2fabd7d8dab6c0d!2sThrilok+IT+Services!5e0!3m2!1sen!2sin!4v1555407199078!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>            </div>
+        </div>
+    </div>-->
+
+    
+          <footer class="default-padding-top text-light" style="background-image: url(img/footer.jpg");>
+                                                           
+                                                           
+                                                           
+        <div class="container">
+            <div class="row">
+                <div class="f-items fv1">
+                    <div class="col-md-4 item">
+                        <div class="f-item">
+                            <h4>
+                            Thrilok It Services</h4>
+                <img src="img/logo.png"style="height:200px;">
+                        </div>
+                    </div>
+                    <div class="col-md-4 item">
+                        <div class="f-item link full">
+                            <h4>Our Support</h4>
+                            <ul>
+                                <li><a href="web-development.html">Web Development</a></li>
+                                <li><a href="ui-ux-designing.html">Ui Ux Designing</a></li>
+                                <li><a href="digital-marketing.html">Digital Marketing</a></li>
+                                <li><a href="mobile-app-development.html">App Development</a></li>
+                                <li><a href="games.html">Game Development</a></li>
+                               <!--<li><a href="it-trainings.html">IT Training</a></li>-->
+                               
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-4 item">
+                        <div class="f-item business-hours">
+                            <h4>Business Hours</h4>
+                            <p>
+                                 We are open six days a week and 9 hours a day.
+                            </p>
+                            <ul>
+                                <li>Monday-Friday <span>9am to 5pm</span></li>
+                                <li>Saturday <span>9am to 2pm</span></li>
+                                <li>Sunday <span>Closed</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                  
+                            <ul>
+                               <p style="text-align:center !important;">&copy; Copyright 2020 <a href="#">Thrilok IT Service</a></p>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+       
+    </footer>
+
+
+    <script src="js/plugins.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
